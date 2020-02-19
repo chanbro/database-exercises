99b9bbab0113ef16e8a0ad0adfb5d709526af955
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-19 15:33:26 -0600

    albums exercise

---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT * FROM albums WHERE release_date >= 1990;
+SELECT * FROM albums WHERE sales <= 20;
+SELECT * FROM albums WHERE genre = 'rock';
