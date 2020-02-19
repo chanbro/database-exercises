cf6b089440af17a7178532c49d7feebe7b62700d
Chaney <52712847+chanbro@users.noreply.github.com>
2020-02-19 11:19:38 -0600

    completed table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR (50),
+    PRIMARY KEY (id)
+);
